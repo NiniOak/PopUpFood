2eb250a7e6b764a26a00ef90e1d19ca6042b67fe
AlexD93 <odniprovskti@hotmail.com>
2017-02-21 22:00:56 -0500

    added a view with an add and cancel buttons between Start Selling and selling button

---
+++ PopupFoodLoginScreen/BeforeStartSellingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BeforeStartSellingViewController.swift
+//  PopupFood
+//
+//  Created by Student on 2017-02-21.
+//  Copyright © 2017 Anita Conestoga. All rights reserved.
+//
+
+import UIKit
+
+class BeforeStartSellingViewController: UIViewController{
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+//before start selling ViewController with the X and + button
+/*func startSelling() {
+ let storyboard = UIStoryboard(name: "startSelling", bundle: nil)
+ let controller = storyboard.instantiateViewController(withIdentifier: "startSelling") as UIViewController
+ 
+ self.present(controller, animated: true, completion: nil)
+ }*/
