3f9d57e451ca77903afc381ebd91892e4d0e694a
Barbara Akaeze <barbaraakaeze@Students-MacBook-Pro-4.local>
2017-04-10 22:31:19 -0400

    Updated chat mesages

---
+++ PopupFoodLoginScreen/ChatMessageCell.swift
@@ -0,0 +1,59 @@
+//
+//  chatMessageCell.swift
+//  PopupFood
+//
+//  Created by Anita on 2017-03-17.
+//  Copyright © 2017 Anita Conestoga. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageCell: UICollectionViewCell {
+    
+    let textView: UITextView = {
+        let tv = UITextView()
+        tv.text = "Let this be sample messages"
+        tv.font = UIFont.systemFont(ofSize: 16)
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = UIColor.clear
+//        tv.textColor = .white
+        return tv
+    }()
+    
+    //This code creates the bubble where the chat messages will be displayed in
+    let bubbleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0, green: 191, blue: 255, alpha: 1)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 16
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    var bubbleWidthAnchor: NSLayoutConstraint?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(bubbleView)
+        addSubview(textView)
+        //BubbleView COnstraints
+        //Constants serves as padding to push text left or right
+        bubbleView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        bubbleView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bubbleWidthAnchor = bubbleView.widthAnchor.constraint(equalToConstant: 200)
+        bubbleWidthAnchor?.isActive = true
+        bubbleView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        
+        //textView Constraints
+        textView.leftAnchor.constraint(equalTo: bubbleView.leftAnchor, constant: 8).isActive = true
+        textView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        textView.rightAnchor.constraint(equalTo: bubbleView.rightAnchor).isActive = true
+        textView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
